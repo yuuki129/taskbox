e8ffb71c08ac28b3c1bf17b447893e48307c870a
xxx <yuuki129@outlook.com>
2023-10-12 18:32:23 +0800

    简单组件

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,16 @@
 /** @type { import('@storybook/vue3-vite').StorybookConfig } */
 const config = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  // stories: [
+  //   "../src/**/*.mdx",
+  //   "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  // ],
+  stories: ['../src/components/**/*.stories.js'],
   staticDirs: ["../public"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
+    '@storybook/addon-a11y',
   ],
   framework: {
     name: "@storybook/vue3-vite",
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,5 @@
+import '../src/index.css';
+
 /** @type { import('@storybook/vue3').Preview } */
 const preview = {
   parameters: {
